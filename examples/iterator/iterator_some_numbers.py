70719061a24dfac6a21bf9bca25d037fdc763d34
Erick Salas-Chaverri <ericksc@gmail.com>
2019-08-18 15:38:46 -0600

    for examples

---
+++ examples/iterator/iterator_some_numbers.py
@@ -0,0 +1,10 @@
+item_i = [i for i in [0,1,2,4,8,16,32,63,128,256]]
+item_j = [i for i in [0,1,2,4,8,16,32,63,128,256]]
+item_k = [i for i in [0,1,2,4,8,16,32,63,128,256]]
+
+
+for i in item_i:
+    for j in item_j:
+        for k in item_k:
+            print(i,j,k)
+pass
