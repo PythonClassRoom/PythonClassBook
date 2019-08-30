274032fc8af538946304b613f128492fcb3c91f4
Erick Salas-Chaverri <ericksc@gmail.com>
2019-08-30 16:38:50 -0600

    for examples

---
+++ examples/json/json_example_2.py
@@ -0,0 +1,14 @@
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
---
+++ examples/json/json_example_3.py
@@ -0,0 +1,16 @@
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
