65a3228e89c86ef118a053240302ee643dd49b09
Erick Salas-Chaverri <ericksc@gmail.com>
2019-09-13 11:32:43 -0600

    Add files via upload

---
+++ examples/File input output/Write to file/write_to_file.py
@@ -0,0 +1,9 @@
+zoo = ['lion', "elephant", 'monkey']
+
+if __name__ == "__main__":
+    f = open("output.txt", add modifier)
+
+    for i in zoo:
+        add the whole zoo to the output.txt
+
+    close the file
